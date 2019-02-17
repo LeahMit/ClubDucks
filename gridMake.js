12b297437ce66d477d696aef08e04edd18f2cd89
John A. V. Cinquegrana <jcinqueg@stevens.edu>
2019-02-16 20:17:29 -0500

    arrow is now drawn

--- gridMake.js
+++ gridMake.js
@@ -1,5 +1,5 @@
 
-function generate( grid ) {
+function generate( grid , start) {
 
     //Full Div container to send everything to
     var gen = document.getElementById("Grid");
@@ -7,8 +7,7 @@ function generate( grid ) {
     //Establishing box dimensions
     var height = grid.getHeight();
     var width = grid.getWidth();
-    var imgLength = getImgLength(width+1, height+1).toString() + "px";
-    console.log( imgLength );
+    var imgLength = getImgLength(width+1, height+1).toString();
 
     for( let y = 0; y < height; y++ ) {
         let row = document.createElement("div") 
@@ -16,12 +15,23 @@ function generate( grid ) {
         for( let x = 0; x < width; x++) {
             let box = document.createElement("img");
             box.src = grid.getBox(x,y);
-            box.style.width = imgLength;
-            box.style.height = imgLength;
+            box.style.width = imgLength + "px";
+            box.style.height = imgLength + "px";
             row.appendChild(box);
         }
         gen.appendChild(row);
     }
+
+    var arrow = document.createElement("img");
+    arrow.id = "arrow";
+    arrow.src = "arrow.png";
+    arrow.style.position = "absolute";
+    arrow.style.top = (start.getY() * imgLength).toString() + "px";
+    console.log( arrow.style.top );
+    arrow.style.left = (start.getX() * imgLength).toString() + "px";
+    console.log( arrow.style.left );
+    gen.appendChild(arrow);
+
 }
 
 /**
@@ -33,14 +43,18 @@ function getImgLength(gridWidth, gridHeight) {
     return Math.min( (window.innerWidth-350) / gridWidth, (window.innerHeight-200) / gridHeight) - 5;
 }
 
-function initArrow(x, y) {
-
+function moveArrow(x, y, imgLength) {
+    var arrow = document.createElement("img");
+    arrow.style.position = "absolute";
+    arrow.style.top = y*imgLength;
+    arrow.style.left = x * imgLength;
+    document.getElementById("Grid").appendChild(arrow);
 }
 
 /**
  * A class containing the puzzle grids to be made
  */
-class grid {
+class Grid {
 
     constructor(array) {
         console.log("Works so far");
@@ -63,12 +77,36 @@ class grid {
 
 }
 
+/**
+ * Class point used to store Cartesian coordinate values
+ */
+class Point {
+
+    constructor(x1, y1) {
+        this.x = x1;
+        this.y = y1;
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
+    }
+
+    toString() {
+        return "This is a point";
+    }
+
+}
+
 window.onload = () => {
     // Once our window is loaded, we generate the first grid
-    console.log("Hey it's not that bad");
-    generate( new grid( [ ["blocked.png", "unblocked.png", "stop.png", "unblocked.png", "unblocked.png"],
+    generate( new Grid( [ ["blocked.png", "unblocked.png", "stop.png", "unblocked.png", "unblocked.png"],
     [ "blocked.png", "blocked.png", "blocked.png", "blocked.png", "unblocked.png" ],
     ["blocked.png", "unblocked.png", "unblocked.png", "blocked.png", "unblocked.png"],
     ["blocked.png", "unblocked.png", "unblocked.png", "unblocked.png", "unblocked.png"],
-    ["start.png", "unblocked.png", "blocked.png", "unblocked.png", "unblocked.png"] ] ) );
+    ["start.png", "unblocked.png", "blocked.png", "unblocked.png", "unblocked.png"] ] ) , //End first argument
+    new Point( 0.0, 4.0 ) );
  };
