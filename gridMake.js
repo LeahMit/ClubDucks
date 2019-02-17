481a9cce248c5cf348e52c6557e1b9f528b94940
John A. V. Cinquegrana <jcinqueg@stevens.edu>
2019-02-16 20:46:08 -0500

    begginnings of animation

--- gridMake.js
+++ gridMake.js
@@ -7,7 +7,7 @@ function generate( grid , start) {
     //Establishing box dimensions
     var height = grid.getHeight();
     var width = grid.getWidth();
-    var imgLength = getImgLength(width+1, height+1).toString();
+    var imgLength = grid.getImgLength();
 
     for( let y = 0; y < height; y++ ) {
         let row = document.createElement("div") 
@@ -15,40 +15,45 @@ function generate( grid , start) {
         for( let x = 0; x < width; x++) {
             let box = document.createElement("img");
             box.src = grid.getBox(x,y);
-            box.style.width = imgLength + "px";
-            box.style.height = imgLength + "px";
+            box.style.width = imgLength.toString() + "px";
+            box.style.height = imgLength.toString() + "px";
             row.appendChild(box);
         }
         gen.appendChild(row);
     }
 
+
+    //Creating and giving the start arrow it's base attributes
     var arrow = document.createElement("img");
     arrow.id = "arrow";
     arrow.src = "arrow.png";
+    arrow.style.width = (imgLength-6) + "px";
+    arrow.style.height = (imgLength-6) + "px";
     arrow.style.position = "absolute";
-    arrow.style.top = (start.getY() * imgLength).toString() + "px";
+
+    //Positioning the start arrow
+    arrow.style.top = (start.getY() * imgLength + 18).toString() + "px"; //15 is added due to the #Grid's padding
+    //+3 due to image size difference
     console.log( arrow.style.top );
-    arrow.style.left = (start.getX() * imgLength).toString() + "px";
+    arrow.style.left = (start.getX() * imgLength + 18).toString() + "px";
     console.log( arrow.style.left );
     gen.appendChild(arrow);
 
+    moveRight( new Point( start.getX()+1, start.getY() ).absoluteX() );
+
 }
 
-/**
- * Returns proper image width based on dimensions of the grid
- * @param {*} gridWidth The width of the grid
- * @param {*} gridHeight The height of the grid
- */
-function getImgLength(gridWidth, gridHeight) {
-    return Math.min( (window.innerWidth-350) / gridWidth, (window.innerHeight-200) / gridHeight) - 5;
-}
-
-function moveArrow(x, y, imgLength) {
-    var arrow = document.createElement("img");
-    arrow.style.position = "absolute";
-    arrow.style.top = y*imgLength;
-    arrow.style.left = x * imgLength;
-    document.getElementById("Grid").appendChild(arrow);
+function moveRight(xNext) {
+    var x = document.getElementById("arrow").style.left;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (xNext > x) {
+        clearInterval(id);
+        } else {
+            x++
+            document.getElementById("arrow").style.left = x;
+        }
+    }
 }
 
 /**
@@ -61,6 +66,11 @@ class Grid {
         this.boxes = array;
         this.height = array.length;
         this.width = array[0].length;
+        this.imgLength = Math.min( (window.innerWidth-350) / (this.width+1), (window.innerHeight-200) / (this.height+1)) - 5;
+    }
+
+    getImgLength(gridWidth, gridHeight) {
+        return this.imgLength;
     }
 
     getHeight() {
@@ -93,6 +103,10 @@ class Point {
 
     getY() {
         return this.y;
+    }
+
+    absoluteX(grid) {
+        return this.x * getImgLength() + 18;
     }
 
     toString() {
