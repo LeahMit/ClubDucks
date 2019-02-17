90b7ccbc0f910545c78664b62dbed8142f54d564
John A. V. Cinquegrana <jcinqueg@stevens.edu>
2019-02-16 19:43:00 -0500

    Grid can now be drawn in full

--- gridMake.js
+++ gridMake.js
@@ -1,6 +1,4 @@
 
-var grid;
-
 function generate( grid ) {
 
     //Full Div container to send everything to
@@ -9,27 +7,33 @@ function generate( grid ) {
     //Establishing box dimensions
     var height = grid.getHeight();
     var width = grid.getWidth();
-    gridSize = height;
-    var array = [];
+    var imgLength = getImgLength(width+1, height+1).toString() + "px";
+    console.log( imgLength );
 
     for( let y = 0; y < height; y++ ) {
         let row = document.createElement("div") 
         row.className = "row";
         for( let x = 0; x < width; x++) {
             let box = document.createElement("img");
-            box.src = "unblocked.png";
+            box.src = grid.getBox(x,y);
+            box.style.width = imgLength;
+            box.style.height = imgLength;
             row.appendChild(box);
         }
         gen.appendChild(row);
     }
 }
 
-function gridSize(side) {
-    var windowHeight = window.innerHeight;
-    var windowWidth = window.innerWidth;
-    var workingHeight = windowHeight - 200;
-    var imgLength = workingHeight / side;
+/**
+ * Returns proper image width based on dimensions of the grid
+ * @param {*} gridWidth The width of the grid
+ * @param {*} gridHeight The height of the grid
+ */
+function getImgLength(gridWidth, gridHeight) {
+    return Math.min( (window.innerWidth-350) / gridWidth, (window.innerHeight-200) / gridHeight) - 5;
+}
 
+function initArrow(x, y) {
 
 }
 
@@ -62,9 +66,9 @@ class grid {
 window.onload = () => {
     // Once our window is loaded, we generate the first grid
     console.log("Hey it's not that bad");
-    generate( new grid( [ ["blocked", "empty", "end", "empty", "empty"],
-    [ "blocked", "blocked", "blocked", "blocked", "empty" ],
-    ["blocked", "empty", "empty", "blocked", "empty"],
-    ["blocked", "empty", "empty", "empty", "empty"],
-    ["start", "empty", "blocked", "empty", "empty"] ] ) );
+    generate( new grid( [ ["blocked.png", "unblocked.png", "stop.png", "unblocked.png", "unblocked.png"],
+    [ "blocked.png", "blocked.png", "blocked.png", "blocked.png", "unblocked.png" ],
+    ["blocked.png", "unblocked.png", "unblocked.png", "blocked.png", "unblocked.png"],
+    ["blocked.png", "unblocked.png", "unblocked.png", "unblocked.png", "unblocked.png"],
+    ["start.png", "unblocked.png", "blocked.png", "unblocked.png", "unblocked.png"] ] ) );
  };
